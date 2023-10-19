b31793ed84ab270daa50effdc99c38a011ca847e
MarioNadal <220056@iessanalberto.com>
2023-10-19 11:31:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Suma from './Suma.js';
+import Resta from './Resta.js';
+import Multiplicacion from './Multiplicacion';
+import Division from './Division';
 
 function App() {
+  const[a] = useState(0);
+  const[b] = useState(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Suma a={a} b={b} />
+      <Resta a={a} b={b} />
+      <Multiplicacion a={a} b={b} />
+      <Division a={a} b={b} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Resta.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function Resta({a,b}) {
+    const [valorA, setValorA] = useState(a);
+    const [valorB, serValorB] = useState(b);
+    const [resultado, setResultado] = useState(0);
+
+    function input1EnteredHandler(event){
+        const nuevoValorA = parseInt(event.target.value);
+        setValorA(nuevoValorA)
+        setResultado(nuevoValorA - valorB)
+    };
+    function input2EnteredHandler(event){
+        const nuevoValorB = parseInt(event.target.value);
+        serValorB(nuevoValorB)
+        setResultado(valorA - nuevoValorB)
+    };
+
+    return(
+    <div><input type="number" value={valorA} onChange={input1EnteredHandler}/>
+        <text>-</text>
+        <input type="number" value={valorB} onChange={input2EnteredHandler}/>
+        <text>=</text>
+        <text>{resultado}</text>
+    </div>)
+};
+
+export default Resta
